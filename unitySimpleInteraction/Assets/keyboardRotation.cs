1a823114c320752ff573d5c3258681118b7839f5
baifang <baifang@gmail.com>
2015-03-26 02:48:14 -0700

    Added keyboard rotation.

---
+++ unitySimpleInteraction/Assets/keyboardRotation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class keyboardRotation : MonoBehaviour {
+	#region pubic data
+	public Vector3 rotationSteps = new Vector3(5, 5, 5);
+	#endregion
+	#region Unity callbacks
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.W) == true || Input.GetKeyUp(KeyCode.UpArrow) == true) {
+			transform.Rotate(new Vector3(rotationSteps.x, 0, 0));
+		} else if (Input.GetKeyUp (KeyCode.S) == true || Input.GetKeyUp(KeyCode.DownArrow) == true) {
+			transform.Rotate(new Vector3(-rotationSteps.x, 0, 0));
+		} else if (Input.GetKeyUp (KeyCode.A) == true || Input.GetKeyUp(KeyCode.LeftArrow) == true) {
+			transform.Rotate(new Vector3(0, rotationSteps.y, 0));
+		} else if (Input.GetKeyUp (KeyCode.D) == true || Input.GetKeyUp(KeyCode.RightArrow) == true) {
+			transform.Rotate(new Vector3(0, -rotationSteps.y, 0));
+	}
+}
+	#endregion
+}
